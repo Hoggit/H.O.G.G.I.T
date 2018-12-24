a983d1b2d88f946fd98b57769fef2a2eb935298c
Lugghawk <acidictadpole@gmail.com>
2018-12-24 18:23:38 -0500

    group_commands -> communication. spacing in utils.lua

--- lib/utils.lua
+++ lib/utils.lua
@@ -1,22 +1,22 @@
 HOGGIT.randomInList = function(list)
-    local idx = math.random(1, #list)
-    return list[idx]
-  end
+  local idx = math.random(1, #list)
+  return list[idx]
+end
 
 HOGGIT.filterTable = function(t, filter)
-    local out = {}
-    for k,v in pairs(t) do
-      if filter(v) then out[k] = v end
-    end
-    return out
+  local out = {}
+  for k,v in pairs(t) do
+    if filter(v) then out[k] = v end
+  end
+  return out
 end
-  
+
 HOGGIT.listContains = function(list, elem)
-    for _, value in ipairs(list) do
-      if value == elem then
-          return true
-      end
+  for _, value in ipairs(list) do
+    if value == elem then
+      return true
     end
-  
-    return false
+  end
+
+  return false
 end
