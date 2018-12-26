4b256d4dcffef1b028229d49ac3debdbb49f1a2b
Jeremy Smitherman <jeremysmitherman@gmail.com>
2018-12-26 15:17:48 -0600

    Added comments for hoggit modules

--- hoggit/error_handling.lua
+++ hoggit/error_handling.lua
@@ -1,9 +1,16 @@
-HandleError = function(err)
+--- error_handling.lua
+-- Adds intelligence to the way scripting errors in DCS are handled.
+-- Must be included after hoggit.logging module
+
+local HandleError = function(err)
     log("Error in pcall: "  .. err)
     log(debug.traceback())
     return err
 end
-  
+
+--- Adds basic try/catch functionality
+-- @param func unsafe function to call
+-- @param catch the function to call if func fails
 try = function(func, catch)
     return function()
       local r, e = xpcall(func, HandleError)
