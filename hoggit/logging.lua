f6a341a81118c5836aafcaf14e5e51e578ec6c62
Jeremy Smitherman <jeremysmitherman@gmail.com>
2019-01-06 13:21:00 -0600

    Made some changes to logging for dist version vs debug version

--- hoggit/logging.lua
+++ hoggit/logging.lua
@@ -1,15 +1,19 @@
 --- Logging.lua
 -- Logging utility for HOGGIT framework
 
--- Open a log file for use
-logFile = io.open(HOGGIT.log_base..[[\HOGGIT.log]], "w")
+if HOGGIT.debug then
+    -- Open a log file for use
+    logFile = io.open(HOGGIT.log_base..[[\HOGGIT.log]], "w")
+end
 
 --- Write a string to the logfile
 -- @param str The string to write to the log
 function log(str)
     if str == nil then str = 'nil' end
-    if logFile then
+    if HOGGIT.debug and logFile then
         logFile:write("HOGGIT --- " .. str .."\r\n")
         logFile:flush()
+    else
+        env.info("HOGGIT --- " .. str .. "\r\n")
     end
 end
