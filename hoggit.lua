2b54280fd20fb32eabe1a97cc0237cadfe1c450e
Jeremy Smitherman <jeremysmitherman@gmail.com>
2019-01-06 14:27:04 -0600

    Fixed debug condition again

--- hoggit.lua
+++ hoggit.lua
@@ -1,6 +1,12 @@
 -- Development mode.  This module is defined and configured with a base config
 -- in the game install Scripts folder in development mode.
-if trigger.misc.getUserFlag(9999) and HOGGIT and HOGGIT.script_base then
+if trigger.misc.getUserFlag(9999) == 1 and HOGGIT and HOGGIT.script_base then
+    trigger.action.outText("DEBUG MODE ON", 10)
+    HOGGIT.debug = true
+    HOGGIT.debug_text = function(text, time)
+        trigger.action.outText(text, time)
+    end
+
     dofile(HOGGIT.script_base..[[\HOGGIT\hoggit\error_handling.lua]])
     dofile(HOGGIT.script_base..[[\HOGGIT\hoggit\logging.lua]])
     dofile(HOGGIT.script_base..[[\HOGGIT\hoggit\utils.lua]])
@@ -10,16 +16,8 @@ if trigger.misc.getUserFlag(9999) and HOGGIT and HOGGIT.script_base then
 else
     -- The dist version of this framework starts with this file in the minification, so we need to define the top
     -- level module right here.
+    trigger.action.outText("DEBUG MODE OFF", 10)
     HOGGIT = {}
-end
-
-if trigger.misc.getUserFlag(9999) then 
-    trigger.action.outText("DEBUG MODE ON", 10)
-    HOGGIT.debug = true
-    HOGGIT.debug_text = function(text, time)
-        trigger.action.outText(text, time)
-    end
-else
     HOGGIT.debug = false
     HOGGIT.debug_text = function()end
 end
